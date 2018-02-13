610fc7972b56de5c6fd6d3a79c70f238109020a0
6akayas <sayakach6@gmail.com>
2018-02-13 19:24:13 +0800

    Fixed typos on ProjectsController and added links to landing page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,26 +1,26 @@
 class ProjectsController < ApplicationController
   
   def index
-  	@projects = Project.all
+      @projects = Project.all
   end
 
   def show
-  	 @project = Project.find(params[:id])
+      @project = Project.find(params[:id])
   end
 
   def new
-	 @project = Project.new
+	   @project = Project.new
   end
 
   def create
-  	@project = Project.new(project_params)
-  	if @project.save
-  		flash[:success] = "Project card made!"
-  		redirect_to project_oath(@project)
-  	else
-  		flash[:danger] = 'Input Error'
-		  render :action => :new
-	end
+    @project = Project.new(project_params)
+      if @project.save
+          flash[:success] = "Project card made!"
+          redirect_to project_path(@project)
+      else
+          flash[:danger] = 'Input Error'
+          render :action => :new
+      end
   end 
 
   def edit
@@ -29,10 +29,9 @@ class ProjectsController < ApplicationController
   def update
   end
 
-
   private
-  def project_params
-    params.require(:article).permit(:title, :description)
-  end
 
+  def project_params
+    params.require(:project).permit(:title, :description)
+  end
 end
